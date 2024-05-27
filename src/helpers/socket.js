36df4cf2bbec68b97593bdc3f823436df6f48533
Mohamed Ramadan <mrbp25091993@gmail.com>
2024-05-27 14:09:17 +0300

    Socket

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
-import { checkEnvironment } from "./checkEnvironment";
 
-const END_POINT = checkEnvironment();
+const END_POINT =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://strong-mind-software-backend.onrender.com";
 // const END_POINT = "https://strong-mind-software-backend.vercel.app";
 export const socket = io(END_POINT);
