c67baed1c2248b57c2fd4205d1c5844cb16d50d5
Mohamed Ramadan <mrbp25091993@gmail.com>
2024-06-11 23:42:11 +0300

    Fix view match

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -4,5 +4,5 @@ import { io } from "socket.io-client";
 //   process.env.NODE_ENV === "development"
 //     ? "http://localhost:5000"
 //     : "https://strong-mind-software-backend.onrender.com";
-const END_POINT = "https://strong-mind-software-backend.vercel.app";
+const END_POINT = "https://strong-mind-software-backend.onrender.com";
 export const socket = io(END_POINT);
